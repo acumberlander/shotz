1eb19a12c319f981b4f86892501baf31d622690b
Austin Cumberlander <austincumberlander@gmail.com>
2018-10-20 14:02:23 -0500

    added movie section

--- part1/javascripts/main.js
+++ part1/javascripts/main.js
@@ -1,4 +1,11 @@
-const cardBuilder = (locationsArray) => {
+// const initializeApp = () => {
+//     locationBuilder();
+// }
+
+// initializeApp();
+
+// location Builder for locationComponents
+const locationBuilder = (locationsArray) => {
     let domString = '';
     locationsArray.forEach((location) => {
         domString += `<div class= "col-4 d-inline-block">`;
@@ -15,15 +22,46 @@ const cardBuilder = (locationsArray) => {
         domString += `</div>`;
     })
     $('#cards').append(domString);
+};
 
-}
+// movie builder for movieComponents
+const movieBuilder = (moviesArray) => {
+    let domString = '';
+    moviesArray.forEach((movie) => {
+        domString += `<div class="card text-center">`
+        domString += `<div class="card-header">`
+        domString += `    <ul class="nav nav-tabs card-header-tabs">`
+        domString += `    <li class="nav-item">`
+        domString += `        <a class="nav-link active" href="#">${movie.title}</a>`
+        domString += `    </li>`
+        domString += `    </ul>`
+        domString += `</div>`
+        domString += `<div class="card-body">`
+        domString += `<img class="img-thumbnail rounded float-left m-3" src="${movie.image}" alt="${movie.title}">`
+        domString += `    <h3 class="card-title d-flex mt-3">${movie.title}</h3>`
+        domString += `    <p class="card-text d-flex">${movie.genre} | ${movie.year}</p>`
+        domString += `    <p class="card-text d-flex text-left">${movie.description}</p>`
+        domString += `</div>`
+        domString += `</div>`
+    })
+    $('#movies').append(domString);
+};
 
+// loading movie data
+$.get('../db/movie.json')
+.done((data) => {
+    console.log(data);
+    movieBuilder(data.movies);
+})
+.fail((error) => {
+    console.error({error});
+})
 
-// loaded data
+// loading locations data
 $.get('../db/locations.json')
 .done((data) => {
     console.log(data);
-    cardBuilder(data.locations);
+    locationBuilder(data.locations);
 })
 .fail((error) => {
     console.error({error});
